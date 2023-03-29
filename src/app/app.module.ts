186256b009e22f10cfa3ac050916ef926b1b2d9d
vedhasbrave <vedhas.ptl@braveailab.com>
2023-03-29 16:50:05 +0530

    Updates done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,27 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star-component';
 import {HttpClientModule} from '@angular/common/http';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
-import { ProductDetailGuard } from './products/product-detail.guard';
+import { ProductModule } from './products/product.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
       AppComponent,
       WelcomeComponent,
-      ProductListComponent,
-      ConvertToSpacesPipe,
-      StarComponent,
-      ProductDetailComponent,
     ],
   imports: [BrowserModule,
-            FormsModule, 
             HttpClientModule,
             RouterModule.forRoot([
-              { path: 'products', component: ProductListComponent },
-              { 
-                path: 'products/:id',
-                canActivate: [ProductDetailGuard],
-                component: ProductDetailComponent 
-              },
               { path: 'welcome', component: WelcomeComponent },
               { path: '', redirectTo: "welcome", pathMatch: 'full'},
               {path: '**', redirectTo: 'products', pathMatch: 'full'}
             ]),
+            ProductModule,
+            SharedModule,
           ],
   bootstrap: [AppComponent]
 })
